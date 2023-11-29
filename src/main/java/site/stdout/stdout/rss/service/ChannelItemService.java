1bab95db64257f11385140327ae7c896f14341b5
dlworhd <dlwodud821@gmail.com>
2023-11-29 14:55:03 +0900

    chore: Modify Cron Time

--- src/main/java/site/stdout/stdout/rss/service/ChannelItemService.java
+++ src/main/java/site/stdout/stdout/rss/service/ChannelItemService.java
@@ -45,7 +45,7 @@ public class ChannelItemService {
 			DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US)
 	);
 
-	@Scheduled(cron = "* * */12 * * *")
+	@Scheduled(cron = "* * 13 * * *")
 	@Transactional
 	public void saveItems(){
 		ObjectMapper objectMapper = new ObjectMapper();
